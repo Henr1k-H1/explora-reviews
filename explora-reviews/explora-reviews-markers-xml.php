80e919c87e82ea6979d0c37a8f521f44d4e86f45
Henrik <henrik.scharm@gmail.com>
2024-03-21 16:11:02 -0700

    Add files via upload

---
+++ explora-reviews/explora-reviews-markers-xml.php
@@ -0,0 +1,2 @@
+<?php header("Content-type: text/xml"); echo "<markers>";echo '<marker id="0" title="Beverly Hills" name="Peninsula Plaza" type="Other" lat="1.2922871" lng="103.8508504" url="http://" img="http://localhost:8888/wordpress/wp-content/plugins/explora-reviews/core/images/exploraluxia_logo.png"/>';
+    echo "</markers>"; ?>
